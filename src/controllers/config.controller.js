ae6ee02e1bcd1f008195a8a42923be3dac598b3b
tjdcjf1996 <r_louis@naver.com>
2024-12-05 11:18:16 +0900

    feat: 컨트롤러 클래스 추가

---
+++ src/controllers/config.controller.js
@@ -0,0 +1,37 @@
+import ConfigService from '../services/config.services.js';
+
+export class ConfigController {
+  #configService;
+  constructor() {
+    this.#configService = new ConfigService();
+  }
+
+  getConfig = (_, res, next) => {
+    try {
+      const configs = this.#configService.getConfig();
+      return res.status(200).json({ configs });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  setConfig = (req, res, next) => {
+    try {
+      const { ip, port } = req.body;
+      this.#configService.setConfig(ip, port);
+      return res.status(200).json({ message: '정상 등록' });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  deleteConfig = (req, res, next) => {
+    try {
+      const { port } = req.body;
+      const deletedServer = this.#configService.deleteConfig(port);
+      return res.status(200).json({ data: deletedServer });
+    } catch (err) {
+      next(err);
+    }
+  };
+}
